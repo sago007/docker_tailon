2071358067f2f9bfcf7113da93f57e760336a1ad
Poul Sander <poul@unity3d.com>
2016-04-28 14:43:38 +0200

    I cannot see that it helps but we know use exec in the wrapper script

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # BASE_LOG=$(mount | grep /dev/ | awk '{print $3}' | tail -n +4 | sed 's/$/\/*/' | tr "\\n" " ")
 
 echo "Running Tailon as python tailon -b 0.0.0.0:8080 -f /data \"${@:1}\""
-tailon -b 0.0.0.0:8080 -f /data "${@:1}"
+exec tailon -b 0.0.0.0:8080 -f /data "${@:1}"
 
 
